59d66cf32976d45653376c1b4bc458a7383b5297
Ulf <ulfaslak@UAM.local>
2019-03-08 16:08:21 +0100

    size by strength, not display singleton nodes

--- vis.js
+++ vis.js
@@ -97,6 +97,8 @@ var controls = {
   'Link stroke': '#7c7c7c',
   'Label stroke': '#000000',
   'Show labels': false,
+  'Show singleton nodes': false,
+  'Node size by strength': false,
   'Zoom': 1.5,
   'Min. link weight %': 0,
   'Max. link weight %': 100
@@ -129,6 +131,8 @@ f3.addColor(controls, 'Node stroke', controls['Node stroke']).onChange(function(
 f3.addColor(controls, 'Link stroke', controls['Link stroke']).onChange(function(v) { inputtedLinkStroke(v) });
 f3.addColor(controls, 'Label stroke', controls['Label stroke']).onChange(function(v) { inputtedTextStroke(v) });
 f3.add(controls, 'Show labels', false).onChange(function(v) { inputtedShowLabels(v) });
+f3.add(controls, 'Show singleton nodes', false).onChange(function(v) { inputtedShowSingletonNodes(v) });
+f3.add(controls, 'Node size by strength', false).onChange(function(v) { inputtedNodeSizeByStrength(v) });
 f3.add(controls, 'Link width', 0.01, 30).onChange(function(v) { inputtedLinkWidth(v) });
 f3.add(controls, 'Link alpha', 0, 1).onChange(function(v) { inputtedLinkAlpha(v) });
 f3.add(controls, 'Node size', 0, 50).onChange(function(v) { inputtedNodeSize(v) });
@@ -148,7 +152,7 @@ function restart(graph) {
 
   // Make `graph` a window variable that the user can access
   window.graph = graph;
-  
+
   // Start simulation
   simulation
       .nodes(graph.nodes)
@@ -167,26 +171,19 @@ function restart(graph) {
 
   function ticked() {
     context.clearRect(0, 0, width, height);
-
-    
     context.strokeStyle = controls['Link stroke'];
     context.globalAlpha = controls['Link alpha'];
     context.globalCompositeOperation = "destination-over"
     graph.links.forEach(drawLink);
-    
-
     context.globalAlpha = 1.0
     context.strokeStyle = controls['Node stroke'];
     context.lineWidth = controls['Node stroke size'] * controls['Zoom'];
     context.globalCompositeOperation = "source-over"
-    // context.lineWidth *= 2;
     graph.nodes.forEach(drawNode);
     graph.nodes.forEach(drawText);
-    
   }
 
   simulation.alpha(1).restart();
-
 }
 
 handle_url(controls['Path to file (csv or json)']);
@@ -264,7 +261,7 @@ function computeNodeRadii(d) {
 }
 
 function computeLinkStrength(d) {
-  var base_strength = 1 / Math.min(node_degrees[d.source.id], node_degrees[d.target.id])
+  var base_strength = 1 / Math.min(masterNodeDegrees[d.source.id], masterNodeDegrees[d.target.id])
   return (1 / base_strength * d.weight / max_link_width)**(controls['Link strength exponent']) * base_strength
 }
 
@@ -309,19 +306,14 @@ function inputtedReheat(v) {+
 
 // Styling
 function inputtedNodeFill(v) {
-
   window.dr = parseInt(v.slice(1, 3), 16) - parseInt(reference_color.slice(1, 3), 16)
   window.dg = parseInt(v.slice(3, 5), 16) - parseInt(reference_color.slice(3, 5), 16)
   window.db = parseInt(v.slice(5, 7), 16) - parseInt(reference_color.slice(5, 7), 16)
-
   for (var g of d3.keys(active_swatch)) {
-
     if (!isNaN(+g)) {
-
       var r_ = bounce_modulus(parseInt(reference_swatch[g].slice(1, 3), 16) + dr, 0, 255);
       var g_ = bounce_modulus(parseInt(reference_swatch[g].slice(3, 5), 16) + dg, 0, 255);
       var b_ = bounce_modulus(parseInt(reference_swatch[g].slice(5, 7), 16) + db, 0, 255);
-
       active_swatch[g] = '#' + toHex(r_) + toHex(g_) + toHex(b_);
     }
   }
@@ -341,6 +333,26 @@ function inputtedTextStroke(v) {
 }
 
 function inputtedShowLabels(v) {
+  simulation.restart();
+}
+
+function inputtedShowSingletonNodes(v) {
+  restart(shave(graph));
+  simulation.restart();
+}
+
+function inputtedNodeSizeByStrength(v) {
+  if (v) {
+    graph.nodes.forEach(n => {
+      n.size = nodeDegrees[n.id];
+    })
+  }
+  if (!v) {
+    graph.nodes.forEach(n => {
+      n.size = findNode(master_graph, n).size || 1;
+    })
+  }
+  recompute_size_norms(graph)
   simulation.restart();
 }
 
@@ -398,16 +410,17 @@ function inputtedZoom(v) {
   simulation.restart();
 }
 
-var vMinPrev = 0
+var vMinPrev = 0;
+var dv = 0;
 function inputtedMinLinkWeight(v) {
+  dv = v - vMinPrev
   if (shiftDown) {
-    var dv = v - vMinPrev
     controls['Max. link weight %'] = d3.min([100, controls['Max. link weight %'] + dv])
   } else {
     controls['Max. link weight %'] = d3.max([controls['Max. link weight %'], v+1])
   }
   vMinPrev = v
-  restart(shave(_.clone(master_graph)));
+  restart(shave(graph));
 }
 
 var vMaxPrev = 0
@@ -419,7 +432,7 @@ function inputtedMaxLinkWeight(v) {
     controls['Min. link weight %'] = d3.min([controls['Min. link weight %'], v-1])
   }
   vMaxPrev = v
-  restart(shave(_.clone(master_graph)));
+  restart(shave(graph));
 }
 
 
@@ -445,24 +458,28 @@ function handle_url() {
 }
 
 
-function restart_if_valid_JSON(raw_graph) {
+function restart_if_valid_JSON(master_graph) {
+
+  // Make master_graph window accessible
+  window.master_graph = master_graph
+
   // Check for 'nodes' and 'links' lists
-  if (!raw_graph.nodes || raw_graph.nodes.length == 0) {
+  if (!master_graph.nodes || master_graph.nodes.length == 0) {
     swal({text: "Dataset does not have a key 'nodes'", icon: "error"})
     return false
   }
-  if (!raw_graph.links) {
+  if (!master_graph.links) {
     swal({text: "Dataset does not have a key 'links'", icon: "warning"})
   }
 
   // Check that node and link objects are formatted right
-  for (var d of raw_graph.nodes) {
+  for (var d of master_graph.nodes) {
     if (!d3.keys(d).includes("id")) {
       swal({text: "Found objects in 'nodes' without 'id' key.", icon: "error"});
       return false;
     }
   }
-  for (var d of raw_graph.links) {
+  for (var d of master_graph.links) {
     if (!d3.keys(d).includes("source") || !d3.keys(d).includes("target")) {
       swal({text: "Found objects in 'links' without 'source' or 'target' key.", icon: "error"});
       return false;
@@ -470,10 +487,10 @@ function restart_if_valid_JSON(raw_graph) {
   }
 
   // Check that 'links' and 'nodes' data are congruent
-  var nodes_nodes = raw_graph.nodes.map(d => {return d.id});
+  var nodes_nodes = master_graph.nodes.map(d => {return d.id});
   var nodes_nodes_set = new Set(nodes_nodes)
   var links_nodes_set = new Set()
-  raw_graph.links.forEach(l => {
+  master_graph.links.forEach(l => {
     links_nodes_set.add(l.source); links_nodes_set.add(l.source.id)  // Either l.source or l.source.id will be null
     links_nodes_set.add(l.target); links_nodes_set.add(l.target.id)  // so just add both and remove null later (same for target)
   }); links_nodes_set.delete(undefined)
@@ -496,41 +513,36 @@ function restart_if_valid_JSON(raw_graph) {
   }
 
   // Check that attributes are indicated consistently in both nodes and links
-  var count_weight = raw_graph.links.filter(n => { return 'weight' in n }).length
-  if (0 < count_weight & count_weight < raw_graph.links.length) {
+  var count_weight = master_graph.links.filter(n => { return 'weight' in n }).length
+  if (0 < count_weight & count_weight < master_graph.links.length) {
     swal({text: "Found links with and links without 'weight' attribute", icon: "error"});
     return false; 
   } else if (count_weight == 0) {
-    raw_graph.links.forEach(l => {l.weight = 1;})
+    master_graph.links.forEach(l => {l.weight = 1;})
   }
-  var count_group = raw_graph.nodes.filter(n => { return 'group' in n }).length
-  if (0 < count_group & count_group < raw_graph.nodes.length) {
+  var count_group = master_graph.nodes.filter(n => { return 'group' in n }).length
+  if (0 < count_group & count_group < master_graph.nodes.length) {
     swal({text: "Found nodes with and nodes without 'group' attribute", icon: "error"});
     return false; 
   }
-  var count_size = raw_graph.nodes.filter(n => { return 'size' in n }).length
-  if (0 < count_size & count_size < raw_graph.nodes.length) {
+  count_size = master_graph.nodes.filter(n => { return 'size' in n }).length
+  if (0 < count_size & count_size < master_graph.nodes.length) {
     swal({text: "Found nodes with and nodes without 'size' attribute", icon: "error"});
     return false; 
   }
   else if (count_size == 0) {
-    var node_strengths = new DefaultDict(Number)
-    raw_graph.links.forEach(l => {
-      node_strengths[l.source] += valIfValid(l.weight, 1);
-      node_strengths[l.target] += valIfValid(l.weight, 1);
-    });
-    raw_graph.nodes.forEach(n => {n.size = node_strengths[n.id];})
+    master_graph.nodes.forEach(n => {n.size = 1;})
   }
 
   // Check for foreign node and link attributes
   var foreign_nodes_attributes = new Set()
-  raw_graph.nodes.forEach(d => {
+  master_graph.nodes.forEach(d => {
     d3.keys(d).forEach(k => {
       if (!['id', 'size', 'group'].includes(k)) foreign_nodes_attributes.add(k)
     })
   })
   var foreign_links_attributes = new Set()
-  raw_graph.links.forEach(d => {
+  master_graph.links.forEach(d => {
     d3.keys(d).forEach(k => {
       if (!['source', 'target', 'weight'].includes(k)) foreign_links_attributes.add(k)
     })
@@ -542,27 +554,25 @@ function restart_if_valid_JSON(raw_graph) {
     swal({text: "Found unexpected link attribute(s): " + Array.from(foreign_links_attributes).join(", "), icon: "warning"})
   }
 
-  master_graph = raw_graph
-
   // Compute and store global variables
-  compute_graph_globals(master_graph);
+  compute_master_graph_globals();
+
+  // Reset all thresholds ...
+  controls["Min. link weight %"] = 0
+  controls["Max. link weight %"] = 100
 
   // Run the restart if all of this was OK
-  restart(shave(_.clone(master_graph)));
+  restart(_.cloneDeep(master_graph));
 }
 
 
 function restart_if_valid_CSV(raw_input) {
   // Assume header is "source,target(,weight)"
+  var nodes = new Set();
   var links = d3.csvParse(raw_input).map(l => {
+    nodes.add(l.source); nodes.add(l.target);
     return {'source': l.source, 'target': l.target, 'weight': +valIfValid(l.weight, 1)}
   })
-
-  var node_strengths = new DefaultDict(Number)
-  links.forEach(l => {
-    node_strengths[l.source] += valIfValid(l.weight, 1);
-    node_strengths[l.target] += valIfValid(l.weight, 1);
-  });
 
   // Warn against zero links
   var zero_links_count = 0
@@ -578,20 +588,85 @@ function restart_if_valid_CSV(raw_input) {
     swal({text: "Removed " + zero_links_count + " links with weight 0", icon: "warning"})
   }
 
-  master_graph = {'nodes': [], 'links': links}
-  d3.keys(node_strengths).forEach(k => {master_graph.nodes.push({'id': k, 'size': node_strengths[k]})})
+  window.master_graph = {'nodes': [], 'links': links}
+  nodes.forEach(n => {master_graph.nodes.push({'id': n, 'size': 1})})
 
   // Compute and store global variables
-  compute_graph_globals(master_graph);
+  compute_master_graph_globals();
 
-  // Input graph that respects user input percolation boundaries
-  restart(shave(_.clone(master_graph)));
+  // Input graph that respects user input percolation boundaries (that's the purpose of using `shave` here)
+  restart(_.cloneDeep(master_graph));
 }
 
 // Various utilities
 // -----------------
 
-function compute_graph_globals(graph) {
+function findNode(_graph, n) {
+  // First just check if its at the same index
+  if (n.hasOwnProperty('index')) {
+    if (_graph.nodes[n.index].id == n.id) {
+      return _graph.nodes[n.index]
+    }
+  }
+  // And if it's not, loop through to look for it
+  else {
+    for (let _n of _graph.nodes) {
+      if (_n.id == (n.id || n)) {
+        return _n;
+      }
+    }
+  }
+  return undefined;
+}
+
+function findLink(_graph, l) {
+  for (let _l of _graph.links) {
+    if (_l.source.id == l.source && _l.target.id == l.target) {
+      return _l;
+    }
+  }
+  return undefined;
+}
+
+function compute_master_graph_globals() {
+
+  // Compute node size norms
+  recompute_size_norms(master_graph)
+
+  // Compute link width norms
+  max_link_width = d3.max(master_graph.links.map(l => { if (l.weight) { return l.weight } else return 0; }));
+  min_link_width = d3.min(master_graph.links.map(l => { if (l.weight) { return l.weight } else return 1; }));
+
+  if (controls['Link width exponent'] > 0) {
+    link_width_norm = 1 / max_link_width**(controls['Link width exponent'])
+  } else {
+    link_width_norm = 1 / min_link_width**(controls['Link width exponent'])
+  }
+
+  // Sort out node colors
+  var node_groups = new Set(master_graph.nodes.filter(n => 'group' in n).map(n => {return n.group}))
+  for (var g of node_groups) {
+    if (typeof(g) == "string") {
+      active_swatch[g] = g
+    } else {
+      active_swatch[g] = '#'+Math.floor(Math.random()*16777215).toString(16);
+    }
+  }
+  window.reference_swatch = _.cloneDeep(active_swatch)
+  reference_color = controls['Node fill']
+
+  // Immutable node degree (unless strength is toggled)
+  masterNodeDegrees = {}; master_graph.nodes.map(n => masterNodeDegrees[n.id] = 0)
+  master_graph.links.forEach(l => {
+    masterNodeDegrees[l.source] += l.weight || 1;
+    masterNodeDegrees[l.target] += l.weight || 1;
+  });
+
+  // Degree dicrionary to keep track of ACTIVE degrees after thresholds are applied
+  nodeDegrees = _.cloneDeep(masterNodeDegrees)
+}
+
+function recompute_size_norms(graph){
   // Compute node size norms
   max_node_size = d3.max(graph.nodes.map(n => { if (n.size) { return n.size } else return 0; }));
   min_node_size = d3.min(graph.nodes.map(n => { if (n.size) { return n.size } else return 1; }));
@@ -601,47 +676,85 @@ function compute_graph_globals(graph) {
   } else {
     node_size_norm = 1 / min_node_size**(controls['Node size exponent'])
   }
-
-  // Compute link width norms
-  max_link_width = d3.max(graph.links.map(l => { if (l.weight) { return l.weight } else return 0; }));
-  min_link_width = d3.min(graph.links.map(l => { if (l.weight) { return l.weight } else return 1; }));
-
-  if (controls['Link width exponent'] > 0) {
-    link_width_norm = 1 / max_link_width**(controls['Link width exponent'])
-  } else {
-    link_width_norm = 1 / min_link_width**(controls['Link width exponent'])
-  }
-
-  // Sort out node colors
-  var node_groups = new Set(graph.nodes.filter(n => 'group' in n).map(n => {return n.group}))
-  for (var g of node_groups) {
-    if (typeof(g) == "string") {
-      active_swatch[g] = g
-    } else {
-      active_swatch[g] = '#'+Math.floor(Math.random()*16777215).toString(16);
-    }
-  }
-  window.reference_swatch = _.clone(active_swatch)
-  reference_color = controls['Node fill']
-
-  node_degrees = new DefaultDict(Number)
-  graph.links.forEach(l => {
-    node_degrees[l.source] += 1;
-    node_degrees[l.target] += 1;
-  });
 }
 
-function shave(input_graph) {
+
+
+function shave(shaveGraph) {
   // Compute what number a percentage corresponds to
   var interval_range = function(percent) {
     return percent / 100 * (max_link_width - min_link_width) + min_link_width
   }
-  // Shave links
-  var graph_displayed = input_graph
-  graph_displayed['links'] = graph_displayed.links.filter(l => {
-    return (interval_range(controls['Min. link weight %']) <= l.weight) && (l.weight <= interval_range(controls['Max. link weight %']))
-  })
-  return graph_displayed
+
+  // SLIDER MOVES RIGHT
+  if (dv > 0) {
+    // Remove links and update `nodeDegrees
+    shaveGraph['links'] = shaveGraph.links.filter(l => {
+      var keepLink = (interval_range(controls['Min. link weight %']) <= l.weight) && (l.weight <= interval_range(controls['Max. link weight %']))
+      if (!keepLink) {
+        nodeDegrees[l.source.id] -= l.weight || 1;
+        nodeDegrees[l.target.id] -= l.weight || 1;
+      }
+      return keepLink
+    })
+
+    // Remove singleton nodes
+    if (!controls['Show singleton nodes']) {
+      shaveGraph['nodes'] = shaveGraph.nodes.filter(n => {
+        return nodeDegrees[n.id] > 0;
+      })
+    }
+
+    // Resize nodes
+    if (controls['Node size by strength']) {
+      shaveGraph.nodes.forEach(n => {
+        n.size = nodeDegrees[n.id]
+      })
+    }
+  }
+
+  // SLIDER MOVES LEFT
+  else if (dv < 0) {
+    master_graph.links.forEach(l => {
+      // If it's not already in the active graph
+      if (findLink(shaveGraph, l) == undefined) {
+        // And it's within the right threshold
+        if ((interval_range(controls['Min. link weight %']) <= l.weight) && (l.weight <= interval_range(controls['Max. link weight %']))) {
+          // Increment the nodeDegrees
+          nodeDegrees[l.source] += l.weight || 1;
+          nodeDegrees[l.target] += l.weight || 1;
+          // And add it!
+          shaveGraph['links'].push(_.cloneDeep(l))
+        }
+      }
+    })
+
+    // Add nodes back
+    if (!controls['Show singleton nodes']) {
+      d3.keys(nodeDegrees).forEach(n => {
+        if (nodeDegrees[n] > 0 && findNode(shaveGraph, n) == undefined) {
+          let _n = _.clone(findNode(master_graph, n))
+          _n['x'] = width / 2 + (Math.random() - 0.5) * shaveGraph.nodes.length;
+          _n['y'] = height /2 + (Math.random() - 0.5) * shaveGraph.nodes.length;
+          shaveGraph['nodes'].push(_n)
+        }
+      })
+    }
+
+    // Resize nodes
+    if (controls['Node size by strength']) {
+      shaveGraph.nodes.forEach(n => {
+        n.size = nodeDegrees[n.id]
+      })
+    }
+  }
+
+  if (controls['Node size by degree']) {
+    shaveGraph.nodes.map(n => {
+      n.size = newNodeDegrees[n.id];
+    })
+  }
+  return shaveGraph
 }
 
 // Utility functions
