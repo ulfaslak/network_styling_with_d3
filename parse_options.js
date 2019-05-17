ebd658717c93321ec79d1c9f91185e216b8481f7
ulfaslak <ulfjensen@gmail.com>
2019-05-17 13:47:22 +0200

    fix bug with vis launching before controls is updated

--- parse_options.js
+++ parse_options.js
@@ -5,17 +5,16 @@ var this_url =  window.location.href;
 this_url = new URL(this_url);
 
 var this_data = this_url.searchParams.get("data");
-if (!((this_data == "null") || (this_data === null)))
-{
+if (!((this_data == "null") || (this_data === null))) {
     controls['file_path'] = this_data;
 }
 
 var this_config = this_url.searchParams.get("config");
-if ( !((this_config == "null") || (this_config === null)))
-{
-    d3.json(this_config,function(data){
-        Reflect.ownKeys(data).forEach(key => controls[key] = data[key]);
-    });
+if ( !((this_config == "null") || (this_config === null))) {
+    d3.json(this_config, new_controls => {
+    	controls = {...controls, ...new_controls}
+    	vis(controls);
+    })
+} else {
+	vis(controls);
 }
-
-vis(controls);
